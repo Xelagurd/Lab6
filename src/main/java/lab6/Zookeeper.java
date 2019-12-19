4e92030ab46d0f45901d18c8f5f6267035486ee9
DrAlexxD <alexander.drugakov@mail.ru>
2019-12-19 16:46:11 +0300

    Scripted auto-commit on change (2019-12-19 16:46:11) by gitwatch.sh

--- src/main/java/lab6/Zookeeper.java
+++ src/main/java/lab6/Zookeeper.java
@@ -12,5 +12,8 @@ public class Zookeeper {
         ZooKeeper zoo = new ZooKeeper("127.0.0.1:8080",3000,this);
         ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
         ActorRef cacheActor = system.actorOf(Props.create(CacheActor.class));
+
+        
+
     }
 }
