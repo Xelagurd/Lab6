b6f1bccf5c01bdd49d1b0d18a5f837cc77bcb5b4
DrAlexxD <alexander.drugakov@mail.ru>
2019-12-20 00:40:30 +0300

    Scripted auto-commit on change (2019-12-20 00:40:29) by gitwatch.sh

--- src/main/java/lab6/AkkaServer.java
+++ src/main/java/lab6/AkkaServer.java
@@ -11,20 +11,26 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.concurrent.CompletionStage;
+import java.util.concurrent.*;
 
 public class AkkaServer {
     private static int port;
     private static ActorRef storageActor;
+    private static ZooKeeper zoo;
+
     private static final String ROUTES = "routes";
     private static final String LOCALHOST = "localhost";
     private static final String SERVER_ONLINE = "Server online on localhost:";
+    private static final String ZOOKEEPER_HOST = "127.0.0.1:5000";
+    private static final String ZOOKEEPER_SERVERS_DIR = "/servers";
+    private static final String ZOOKEEPER_SERVER_DIR = "/servers/";
+    private static final int TIMEOUT = 5000;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         AkkaServer akkaServer = new AkkaServer();
 
         Scanner in = new Scanner(System.in);
@@ -32,6 +38,20 @@ public class AkkaServer {
 
         ActorSystem system = ActorSystem.create(ROUTES);
         storageActor = system.actorOf(Props.create(StorageActor.class));
+
+        zoo = new ZooKeeper(
+                ZOOKEEPER_HOST,
+                TIMEOUT,
+                new CustomWatcher()
+        );
+        zoo.create(
+                ZOOKEEPER_SERVER_DIR + Integer.toString(port),
+                Integer.toString(port).getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL
+        );
+
+        zoo.getChildren(ZOOKEEPER_SERVERS_DIR, new CustomWatcher());
 
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = akkaServer.route().flow(system, materializer);
